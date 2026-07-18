0ac8cc0f98fd3462015e4c269cf95379ffb3dd1f
Abhaybh025 <bhardwajabhay018@gmail.com>
2026-07-18 22:04:05 +0530

    updated test_cleaner.py

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -1,16 +1,21 @@
 from modules.cleaner import clean_resume_text
-from modules.parser import extract_text_from_pdf
+from modules.parser import extract_text_from_pdf, extract_text_from_txt
 
-resume_text = extract_text_from_pdf(r"data\sample_resume.pdf")
+# resume_text = extract_text_from_pdf(r"data\sample_resume.pdf")
 
-cleaned_text = clean_resume_text(resume_text)
+# cleaned_text = clean_resume_text(resume_text)
 
-print(cleaned_text)
+# print(cleaned_text)
 
-from modules.cleaner import count_words
-word_count = count_words(cleaned_text)
-print(f"Word Count: {word_count}")
+# from modules.cleaner import count_words
+# word_count = count_words(cleaned_text)
+# print(f"Word Count: {word_count}")
 
-from modules.cleaner import get_top_n_words
-top_words = get_top_n_words(cleaned_text, n=10)
-print(f"Top Words: {top_words}")
+# from modules.cleaner import get_top_n_words
+# top_words = get_top_n_words(cleaned_text, n=10)
+# print(f"Top Words: {top_words}")
+
+jd = extract_text_from_txt('./data/jd.txt')
+jd_text = clean_resume_text(jd)
+
+print("Cleaned jd text:", jd_text)
